bfc42d72f953325b69dc08d4ee4f8a81c013f6df
kyle <dev.kyleulman@gmail.com>
2022-10-31 19:12:11 -0400

    update request headers

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,11 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const RES = await fetch(`${import.meta.env.VITE_API_URL}/learn-sveltekit`);
+	const RES = await fetch(`${import.meta.env.VITE_API_URL}/learn-sveltekit`, {
+		headers: {
+			accept: 'application/json'
+		}
+	});
 
 	const RET = await RES.json();
 
--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -2,7 +2,12 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, fetch }) => {
 	const RES = await fetch(
-		`${import.meta.env.VITE_API_URL}/learn-sveltekit/${params.slug}`
+		`${import.meta.env.VITE_API_URL}/learn-sveltekit/${params.slug}`,
+		{
+			headers: {
+				accept: 'application/json'
+			}
+		}
 	);
 
 	const RET = await RES.json();
