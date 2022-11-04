c30bb771ad1a8a6ea3cc55d3991178e1fd7608d1
kyle <dev.kyleulman@gmail.com>
2022-11-04 19:06:11 -0400

    fix request wrapper

--- src/lib/utils/request/index.ts
+++ src/lib/utils/request/index.ts
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 
 export const req = async (
 	url: RequestInfo | URL,
-	options?: RequestInit | undefined,
+	opts?: RequestInit | undefined,
 	fetch?: any
 ) => {
 	try {
@@ -10,22 +10,18 @@ export const req = async (
 			url = `${import.meta.env.VITE_API_URL}${url}`;
 		}
 
-		options = {
-			...options,
-			method: options?.method || 'GET',
+		const options: RequestInit = {
+			method: opts?.method || 'GET',
 			headers: {
 				'content-type': 'application/json',
 				accept: 'application/json',
-				...options?.headers
+				...opts?.headers
 			}
 		};
 
-		console.log(options);
+		if (opts?.body) options.body = opts?.body;
 
-		const RES = await fetch(url, options);
-
-		console.log(RES);
-
+		const RES = await fetch(url, opts);
 		return await RES.json();
 	} catch (err) {
 		throw error(400, { message: err?.toString() || 'Fetch unsuccessful.' });
